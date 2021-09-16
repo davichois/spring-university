22037b80b378ba020a30f983cf30f030190deffb
David Prada Linarez <davidprali27@gmail.com>
2021-09-16 12:52:14 -0500

    realease: new package and points and validation

--- src/main/java/upeu/edu/pe/employees/controller/EmployeeController.java
+++ src/main/java/upeu/edu/pe/employees/controller/EmployeeController.java
@@ -2,12 +2,17 @@ package upeu.edu.pe.employees.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import upeu.edu.pe.employees.entity.Employee;
 import upeu.edu.pe.employees.service.EmployeeService;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/employee")
@@ -22,8 +27,24 @@ public class EmployeeController {
     }
 
     @GetMapping("/{id}")
-    public Employee showOne(@PathVariable("id") Integer id){
-        return employeeService.findById(id);
+    public ResponseEntity<?> showOne(@PathVariable("id") Integer id){
+        Employee employee = null;
+        Map<String, Object> response = new HashMap<>();
+
+        try {
+            employee = this.employeeService.findById(id);
+
+            if (employee == null){
+                response.put("message", "El empleado con ID: " + id.toString() + " no existe en la Base de datos.");
+                return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+            }
+
+            return new ResponseEntity<Employee>(employee, HttpStatus.OK);
+        }catch (DataAccessException e){
+            response.put("message", "Error al realizar la consulta");
+            response.put("error", e.getMessage());
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping("/")
